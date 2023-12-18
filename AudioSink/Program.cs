e29a60c48f51513baf9c507127b03cf09497d47c
Daniel <790119+DanTheMan827@users.noreply.github.com>
2023-12-18 17:43:59 -0600

    Only allow one instance to run at a time

--- AudioSink/Program.cs
+++ AudioSink/Program.cs
@@ -1,5 +1,6 @@
 using AudioSink.Services;
 using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace AudioSink
@@ -7,19 +8,29 @@ namespace AudioSink
     internal static class Program
     {
         internal static WatcherService Watcher = new WatcherService();
-
+        private const string mutexName = "857c80d5-9d2d-4f75-8210-154b0d694fee";
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            using (Mutex mutex = new Mutex(true, mutexName, out bool createdNew))
+            {
+                if (createdNew)
+                {
+                    // To customize application configuration such as set high DPI settings or default font,
+                    // see https://aka.ms/applicationconfiguration.
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new TrayIconContext());
+                    Application.Run(new TrayIconContext());
+                }
+                else
+                {
+                    MessageBox.Show("Only one instance of AudioSink can be run at a time.");
+                }
+            }
         }
     }
 }
